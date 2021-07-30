a85396267e9326ad3a99de571f15bf2882338493
al-burlachenko <alexandrburl@gmail.com>
2021-07-30 23:11:15 +0300

    реализует функционал ДЗ с багами

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-import './sass/main.scss';
+import './css/styles.css';
+import CountriesApiService from './js/countries-api-service';
+
+import countriesListTpl from './templates/countriesList.hbs';
+import countriesInfoTpl from './templates/countriesInfo.hbs';
+
+const _ = require('lodash');
+const DEBOUNCE_DELAY = 300;
+
+const countriesApiService = new CountriesApiService();
+
+const refs = {
+  inputField: document.getElementById('search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.inputField.addEventListener('input', _.debounce(getCountryName, DEBOUNCE_DELAY));
+
+// console.log(refs.countryInfo);
+// refs.inputField.value=>
+
+function getCountryName() {
+  countriesApiService.query = refs.inputField.value.trim();
+  // if (countryName === '') {
+  //   return;
+  // }
+  // countriesApiService.fetchCountries().then(countries => {
+  //   console.log(countries);
+  //   appendCountryList(countries)
+  // });
+
+  countriesApiService.fetchCountries().then(countries => {
+    console.log({countries});
+  });
+
+}
+
+function appendCountryList(countries) {
+  refs.countryList.insertAdjacentHTML('beforeend', countriesListTpl({countries}));
+}
+
+function clearCountryList() {
+  refs.countryList.innerHTML = '';
+}
